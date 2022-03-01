031b3124ea36ba99162d25556e5a32a8fbf7c95e
Dmitry <punkreatine@gmail.com>
2022-03-01 18:20:47 +0300

    Timer

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const birthday = '2022-03-25';
+
+function countdown(){
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+    const totalseconds = Math.floor((birthdayDate - currentDate) / 1000);
+    const seconds = Math.floor(totalseconds % 60);
+    const mins =  Math.floor(totalseconds / 60) % 60;
+    const hours =  Math.floor(totalseconds / 3600) % 24;
+    const days =  Math.floor(totalseconds / 3600 / 24);
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown,1000);
+
+
+
