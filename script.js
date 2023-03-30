eae6f8e702d82f445907cab9fe1d41d8b2fbabb5
Dmitry <65094306+str3sss@users.noreply.github.com>
2023-03-31 00:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
+const monthsEL = document.getElementById('months')
+const yearsEL = document.getElementById('years')
 
-
-const birthday = '2022-11-24T00:00+03:00';
+const birthday = '2003-03-31T00:00+03:00';
 
 function countdown(){
     const birthdayDate = new Date(birthday);
     const currentDate = new Date();
-    const totalseconds = Math.floor((birthdayDate - currentDate) / 1000);
-    const seconds = Math.floor(totalseconds % 60);
-    const mins =  Math.floor(totalseconds / 60) % 60;
+    const totalseconds = Math.floor((currentDate - birthdayDate) / 1000); // секунды
+    const seconds = Math.floor(totalseconds % 60); // округляем сукунды
+    const mins =  Math.floor(totalseconds / 60) % 60; // округляем минуты
     const hours =  Math.floor(totalseconds / 3600) % 24;
-    const days =  Math.floor(totalseconds / 3600 / 24);
-
-
+    const days =  Math.floor((totalseconds / 3600 / 24) % 365.25)
+    const months = Math.floor(totalseconds / 3600 / 30.43058812 / 24) % 12
+    const years = Math.floor(totalseconds / 3600 / 30.43058812 / 24 / 12)
+    
+    
+    yearsEL.innerHTML = years
+    monthsEL.innerHTML = months
     daysEl.innerHTML = days;
     hoursEl.innerHTML = hours;
     minsEl.innerHTML = mins;
@@ -25,6 +30,9 @@ function countdown(){
 countdown();
 
 setInterval(countdown,1000);
+
+
+
 
 
 
